eaed13f0fd47cac9bb359f73c18e25d19531f668
Sckende <clrccljuhasz@gmail.com>
2018-04-17 13:29:42 -0300

    New script to analyse TT data

---
+++ TinyTags/2017_TinyTags_analyses.R
@@ -0,0 +1,6 @@
+#~~~~~~~~~~~~~~~~~~~~~~~ TINY TAGS ~~~~~~~~~~~~~~~~~~#
+# Only 2017 data
+# Extraction of recesses frequence, mean and ... did by Laurent Montagano (script : " ... " and data file: "LAREN_inc_geese.csv" )
+# Field information of TinyTags are contained in "2017_TT_infos.csv"
+
+setwd("/home/claire/OneDriveDoc/Doc doc doc/Ph.D. - ANALYSES/R analysis/Data")
