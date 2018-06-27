ac5b5aaf4633d99ab07c5aeb33fd933593b7b2f0
Sckende <clrccljuhasz@gmail.com>
2018-06-27 15:51:04 -0400

    Modeles avec nouvelle BDD et dates exactes

--- MARK method/HATCH_DATE_TEST.R
+++ MARK method/HATCH_DATE_TEST.R
@@ -2,94 +2,77 @@ getwd()
 setwd("C:/Users/HP_9470m/OneDrive - Université de Moncton/Doc doc doc/Ph.D. - ANALYSES/R analysis/Data")
 
 rm(list = ls())
+list.files()
 
-gsg <- read.table("GOOSE_MARK_all_nests_all_years.txt", h = T, dec = ".", sep = "\t") # all years, with and without supplemented nests
-str(gsg)
-dim(gsg)
-summary(gsg) 
+h <- read.table("GOOSE-Supl_Nests_2015-2016-2017_LAST_version.txt", h = T, dec = ".", sep = "\t") # all years, with and without supplemented nests
+str(h)
+dim(h)
+summary(h) 
 
-#################### Cleaning of data ####################
+#################### Data subset ####################
+# Keep only true dates of initiation and hatching
+hh <- subset(h, h$INI_STATUS == "TRUE")
+hh <- subset(hh, hh$HATCH_STATUS == "TRUE" | hh$HATCH_STATUS == "MARIE_EST/TRUE")
 
-gsg$HABITAT[gsg$HABITAT == "Mesic"] <- "MES"
-gsg$HABITAT[gsg$HABITAT == "Wetland"] <- "WET"
+# Keep only the successfull nests
+hh <- subset(hh, hh$NIDIF == "S")
+hh <- droplevels(hh)
+summary(hh)
 
-gsg$SUPPL[gsg$SUPPL == "FOO"] <- "F"
-gsg$SUPPL[gsg$SUPPL == "WAT"] <- "W"
-
-gsg$Groupe[gsg$Groupe == "Colony"] <- "COLONY"
-
-gsg <- droplevels(gsg)
-# Formating the reference level
-gsg$SUPPL <- relevel(gsg$SUPPL, "TEM")
-
-# Delete WF level in SUPPL
-gsg <- gsg[gsg$SUPPL != "WF",]
-gsg <- gsg[gsg$SUPPL != "NONE",]
-
-# Nest ISSUE 0:excluded, 1:Success, 2:Abandonment, 3:Destroyed, 5:Unknown
-gsg$Fate[gsg$Groupe == "COLONY" & gsg$ISSUE == 1] <- 0
-gsg$Fate[gsg$Groupe == "COLONY" & gsg$ISSUE == 2 | gsg$ISSUE == 3] <- 1
-
-
-# Delete the ISSUE variable
-gsg <- gsg[,-11]
-
-gsg$Fate <- as.factor(gsg$Fate)
-gsg <- droplevels(gsg)
-
-# Remove NAs
-gsg$SupplDate[is.na(gsg$SupplDate)] <- 99999
-gsg <- na.omit(gsg)
-
+hh$INITIATION <- as.numeric(as.character(hh$INITIATION))
+hh$HATCH <- as.numeric(as.character(hh$HATCH))
 
 #### Supplementation effect on the hatching date ####
-# Using gsg database after cleaning code
-# Keep only experimental years, i.e. 2015, 2016, 2017
-try <- gsg[gsg$AN == 2015 | gsg$AN == 2016 | gsg$AN == 2017,]
-
-# Keep only nest with success
-try <- try[try$Fate == 0,]
-
-# Check if all lastpresent date = lastchecked date
-all(try$LastChecked %in% try$LastPresent)
-# More right to use
-identical(try$LastChecked, try$LastPresent)
 
 # Add rainfall year type (LOW/INTERMEDIATE/HIGH)
 cum <- read.table("PREC_cum2.txt", dec = ".", h = T, sep = " ")
 summary(cum)
-try$RAINFALL <- cum$RAINFALL[match(try$AN, cum$YEAR)]
+hh$RAINFALL <- cum$RAINFALL[match(hh$YEAR, cum$YEAR)]
 
-summary(try)
+summary(hh)
 
 # Add PER CAPITA the cumulative rainfall, cumulative rainfall per day and mean temperature from initiation date to hatching date 
-
-# WARNING ! Here the LastChecked is considered as the hatching date. Could be more acurate ! 
 
 rain <- read.table("PREC_precipitation_Bylot_1995-2017.txt", h = T, dec = ","); rain <- na.omit(rain)
 temp <- read.table("TEMP_Tair moy 1989-2017 BYLCAMP.txt", h = T, dec = ",")
 
-try$ID <- 1:4248
-try$AN <- as.numeric(try$AN)
-for (i in unique(try$AN)) {
-  try$
-}
-#### A FINIR ####
+# Date conversion for temp data 
+temp$JJ <- strptime(paste(temp$DAY, temp$MONTH, temp$YEAR, sep = "-"), format = "%d-%m-%Y")
+temp$JJ <- temp$JJ$yday + 1 #c
+
+hh$ID <- 1:53
+TAB <- NULL
+for (i in hh$ID) {
+  
+  ID <- i
+  INCUB <- hh$HATCH[i] - hh$INITIATION[i]
+  cumPREC <- sum(rain$RAIN[rain$YEAR == hh$YEAR[i] & rain$JJ <= hh$HATCH[i] & rain$JJ >= hh$INITIATION[i]])
+  PREC_rate <- sum(rain$RAIN[rain$YEAR == hh$YEAR[i] & rain$JJ <= hh$HATCH[i] & rain$JJ >= hh$INITIATION[i]]) / INCUB
+  mean_TEMP <- mean(temp$TEMP[temp$YEAR == hh$YEAR[i] & temp$JJ <= hh$HATCH[i] & temp$JJ >= hh$INITIATION[i]])
+  sd_TEMP <- sd(temp$TEMP[temp$YEAR == hh$YEAR[i] & temp$JJ <= hh$HATCH[i] & temp$JJ >= hh$INITIATION[i]])
+  
+  M <- c(ID, INCUB, cumPREC, PREC_rate, mean_TEMP, sd_TEMP)
+  TAB <- as.data.frame(rbind(TAB, M))
+  }
+  View(TAB)
+  names(TAB) <- c("ID", "INCUB", "cumPREC", "PREC_rate", "mean_TEMP", "sd_TEMP")
+  
+  hh <- merge(hh, TAB, by ="ID")
 
 # MODELES
 
-plot(try$LastChecked)
-hist(try$LastChecked)
+plot(hh$INCUB)
+hist(hh$INCUB)
 
 require(lme4)
 
 # Full model
-l0 <- lmer(LastChecked ~ SUPPL + (1|AN), data = try, REML = F) # REML = F allows comparison between likelihood estimator of different models 
+l0 <- lmer(INCUB ~ SUPPL + HAB + cumPREC + mean_TEMP + (1|YEAR), data = hh, REML = F) # REML = F allows comparison between likelihood estimator of different models 
 l0
 summary(l0)
 
 # Null model
-l.NULL <- lmer(LastChecked ~ 1  + (1|AN), data = try, REML = F)
+l.NULL <- lmer(INCUB ~ cumPREC + (1|YEAR), data = hh, REML = F)
 l.NULL
 summary(l.NULL)
 
