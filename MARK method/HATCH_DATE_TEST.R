8cc0d33287d7a0a7ff252b57e14d2407423103a1
Sckende <clrccljuhasz@gmail.com>
2018-06-28 17:32:22 -0400

    Stepwise comparison models

--- MARK method/HATCH_DATE_TEST.R
+++ MARK method/HATCH_DATE_TEST.R
@@ -59,23 +59,95 @@ for (i in hh$ID) {
   
   hh <- merge(hh, TAB, by ="ID")
 
-# MODELES
-
+#### MODELS - STEPWISE METHOD ####
+  # Distribution of the response variable
 plot(hh$INCUB)
 hist(hh$INCUB)
 
+ # Models tests
 require(lme4)
+require(car)
 
-# Full model
-l0 <- lmer(INCUB ~ SUPPL + HAB + cumPREC + mean_TEMP + (1|YEAR), data = hh, REML = F) # REML = F allows comparison between likelihood estimator of different models 
-l0
+hh$SUPPL <- relevel(hh$SUPPL, ref="TEM") # setting of the reference level
+
+#### Full model ####
+# Not enougth levels to consider a random effect with year (necessity of minimum 5 levels for strong results)
+
+l0 <- lm(INCUB ~ SUPPL + HAB + cumPREC + CLUTCH + mean_TEMP, data = hh); l0
 summary(l0)
 
-# Null model
-l.NULL <- lmer(INCUB ~ cumPREC + (1|YEAR), data = hh, REML = F)
-l.NULL
-summary(l.NULL)
+# To check the global effect of factors
+Anova(l0, type = "III") # library car
+
+#### Model 1 ####
+# Delete mean_TEMP cause non significant
+l1 <- lm(INCUB ~ SUPPL + HAB + cumPREC + CLUTCH, data = hh); l1
+summary(l1)
+Anova(l1, type = "III")
+# Model comparison
+anova(l1, l0) #always in first the most simple model, here l1
+# Non significant anova, so, models are similar and mean_TEMP can be deleted
+# Work again as previously with the new best model - l1 - and dele HAB (cf l2)
+
+#### Model 2 *** ####
+l2 <- lm(INCUB ~ SUPPL + cumPREC + CLUTCH, data = hh); l2
+summary(l2)
+
+
+Anova(l2, type = "III")
 
 # Model comparison
-anova(l.NULL, l0)
+anova(l2, l1)
+# Non significant ANOVA, HAB is deleted and all variables are significant. This model is the best one.
 
+
+#### Assumptions tests ####
+
+# Pauline script
+require(effects)
+plot(allEffects(l2)) #from library effects, gives you a (not pretty) plot of the effects of the variables in your model
+
+# Vérification des suppositions du modèle ####
+#homogénéité = graphique des valeurs prédites vs valeurs résiduelles
+plot(x = fitted(l2), 
+     y = resid(l2), 
+     xlab = "Fitted Values",
+     ylab = "Normalized residuals")
+abline(h = 0, lty = 2)
+
+#indépendance (plotter chaque variable vs les résidus)
+plot(x = hh$cumPREC, 
+     y = resid(l2), 
+     xlab = "cumPREC",
+     ylab = "Normalized residuals")
+abline(h = 0, lty = 2)
+
+plot(x = hh$CLUTCH, 
+     y = resid(l2), 
+     xlab = "Clutch size",
+     ylab = "Normalized residuals")
+abline(h = 0, lty = 2)
+
+boxplot(resid(l2) ~ hh$SUPPL,   
+        ylab = "Normalized residuals",
+        data = hh, xlab = "Supplementation")
+abline(h = 0, lty = 2)
+
+
+#normalité
+hist(resid(l2))
+qqnorm(resid(l2))
+qqline(resid(l2))
+
+#leverage (check si y a des valeurs qui tirent les résidus)
+lev <- hat(model.matrix(l2)) # si > 0.2, se questionner 
+plot(lev)
+
+#OU (autre méthode) :
+ggplot(data.frame(lev=hatvalues(l2),pearson=residuals(l2,type="pearson")),
+       aes(x=lev,y=pearson)) +
+  geom_point() +
+  theme_bw() #other way
+hh[lev >0.2,]
+#si valeurs qui tirent, les identifier avec :
+#testmasschg[lev >0.2,] #returns all observations (line number) with leverage values above 0.2
