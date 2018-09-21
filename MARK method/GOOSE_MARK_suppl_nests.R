fbb66db5cdbf025825b0f626b98c0bda14ca9ade
Sckende <clrccljuhasz@gmail.com>
2018-09-21 10:34:05 -0300

    Script pour analyser les nids supplémentés de façon indépendante

---
+++ MARK method/GOOSE_MARK_suppl_nests.R
@@ -0,0 +1,8 @@
+#---------------- GOOSE_MARK_supplemented_nests ----------------#
+# SCript to analyse the DSR of supplemented nests
+# 3 types of models
+#     water vs. control (2005, 2015, 2016, 2017)
+#     food vs. control (2015, 2016, 2017)
+#     food + water vs. control (2015)
+
+
