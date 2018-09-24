dca5a026db610609b09bd215ad2a27c33b0c4b60
Sckende <clrccljuhasz@gmail.com>
2018-09-24 16:14:05 -0300

    WATER modèles avec le jeux de données de Nicolas & WATFOO modèles

--- MARK method/GOOSE_MARK_suppl_nests.R
+++ MARK method/GOOSE_MARK_suppl_nests.R
@@ -8,7 +8,7 @@
 setwd("C:/Users/HP_9470m/OneDrive - Université de Moncton/Doc doc doc/Ph.D. - ANALYSES/R analysis/Data")
 
 rm(list = ls())
-sup <- read.table("GOOSE_geese.txt", sep = ",", h = T)
+sup <- read.table("GOOSE_geese_with_WF.txt", sep = ",", h = T)
 head(sup)
 summary(sup)
 
@@ -74,10 +74,80 @@ WATER.1.results
 
 
 #Save models list and est model
-#save(WATER.1.results, file = "waterGEESE_V1.rda")
-#save(wat8, file = "waterGEESE_V1_1.rda")
-#save(wat9, file = "waterGEESE_V1_2.rda")
+#save(WATER.1.results, file = "waterGEESE_2015-2017.rda")
+#save(wat8, file = "waterGEESE_2015-2017_1.rda")
+#save(wat9, file = "waterGEESE_2015-2017_2.rda")
 
+#### Second WATER models - 2005, 2015 to 2017 ####
+w05 <- read.table("GOOSE_Lecomte_supp_nests_2005.txt", h = T )
+#Obtention de la variable AgeDay1 = correspond à l'âge du nid lors du premier jour du suivi de nids
+w05$AgeDay1 <- (w05$AgeFound - w05$FirstFound) + 1
+
+supW1 <- sup[which(sup$YEAR == 2015 | sup$YEAR == 2016 | sup$YEAR == 2017),]
+supW1 <- supW1[which(supW1$SUPPL == "W" | supW1$SUPPL == "TEM"),]
+supW1 <- droplevels(supW1)
+supW11 <- supW1[, c(1:4, 7:10, 14, 16)]
+head(supW11)
+
+supW2 <- rbind(supW11, w05)
+
+supW2$YEAR <- as.factor(supW2$YEAR)
+supW2$Fate <- as.factor(supW2$Fate)
+summary(supW2)
+
+require(RMark)
+
+nocc <- max(supW2$LastChecked)
+
+run.WATER.2 = function()
+{
+  # Null model
+  wat1 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ 1)), delete = T)
+  
+  # YEAR
+  wat2 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ YEAR)), groups = "YEAR", delete = T)
+  
+  #SUPPL
+  wat3 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL)), groups = "SUPPL", delete = T)
+  
+  # HAB
+  wat4 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ HAB)), groups = "HAB", delete = T)
+  
+  # INITIATION
+  #wat5 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ INITIATION)), delete = T)
+  
+  #### ** WARNING ** --> no initiation variable for 2005 data
+  
+  # NestAge
+  wat6 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ NestAge)), delete = T)
+  
+  # Complete model without interaction
+  #wat7 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL + HAB + YEAR + NestAge + INITIATION)), groups = c("SUPPL", "HAB", "YEAR" ), delete = T)
+  
+  #### ** WARNING ** --> no initiation variable for 2005 data
+  
+  # SUPPL + HAB + YEAR + NestAge
+  wat8 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL + HAB + YEAR + NestAge)), groups = c("SUPPL", "HAB", "YEAR" ), delete = T)
+  
+  # SUPPL*HAB + YEAR + NestAge
+  wat9 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL*HAB + YEAR + NestAge)), groups = c("SUPPL", "HAB", "YEAR" ), delete = T)
+  
+  # SUPPL*HAB + YEAR
+  wat10 <- mark(supW2, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL*HAB + YEAR)), groups = c("SUPPL", "HAB", "YEAR" ), delete = T)
+  
+  
+  return(collect.models() )
+}
+
+# run defined models
+WATER.2.results <- run.WATER.2()
+WATER.2.results
+
+
+#Save models list and est model
+#save(WATER.2.results, file = "waterGEESE_2005-2017.rda")
+#save(wat8, file = "waterGEESE_2005-2017_1.rda")
+#save(wat9, file = "waterGEESE_2005-2017_2.rda")
 
 #### Food models ####
 # Concerning years : 2015, 2016 & 2017
@@ -146,9 +216,69 @@ FOOD.results
 
 
 #Save models list and est model
-save(FOOD.results, file = "foodGEESE.rda")
-save(foo8, file = "foodGEESE_1.rda")
-save(foo7, file = "foodGEESE_2.rda")
-save(foo9, file = "foodGEESE_3.rda")
-save(foo12, file = "foodGEESE_4.rda")
-save(foo11, file = "foodGEESE_5.rda")
+#save(FOOD.results, file = "foodGEESE.rda")
+#save(foo8, file = "foodGEESE_1.rda")
+#save(foo7, file = "foodGEESE_2.rda")
+#save(foo9, file = "foodGEESE_3.rda")
+#save(foo12, file = "foodGEESE_4.rda")
+#save(foo11, file = "foodGEESE_5.rda")
+
+
+#### WATER/FOOD models ####
+# Only concerning 2015
+
+supWF <- sup[which(sup$YEAR == 2015),]
+supWF <- supWF[which(supWF$SUPPL == "WF" | supWF$SUPPL == "TEM"),]
+supWF <- droplevels(supWF)
+summary(supWF)
+
+supWF$YEAR <- as.factor(supWF$YEAR)
+supWF$Fate <- as.factor(supWF$Fate)
+supWF$SUPPL <- relevel(supWF$SUPPL, "TEM")
+
+require(RMark)
+
+nocc <- max(supWF$LastChecked)
+
+run.WATFOO = function()
+{
+  # Null model
+  watfoo1 <- mark(supWF, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ 1)), delete = T)
+  
+  # SUPPL
+  watfoo2 <- mark(supWF, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL)), groups = "SUPPL", delete = T)
+  
+  # HAB
+  watfoo3 <- mark(supWF, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ HAB)), groups = "HAB", delete = T)
+  
+  # INITIATION
+  watfoo4 <- mark(supWF, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ INITIATION)), delete = T)
+  
+  # NestAge
+  watfoo5 <- mark(supWF, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ NestAge)), delete = T)
+  
+  # SUPPL + HAB + NestAge + INITIATION
+  watfoo6 <- mark(supWF, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL + HAB + NestAge + INITIATION)), groups = c("SUPPL", "HAB"), delete = T)
+  
+  # SUPPL + HAB + NestAge
+  watfoo7 <- mark(supWF, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL + HAB + NestAge)), groups = c("SUPPL", "HAB"), delete = T)
+  
+  # SUPPL*HAB + NestAge
+  watfoo8 <- mark(supWF, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL*HAB + NestAge)), groups = c("SUPPL", "HAB"), delete = T)
+  
+  # SUPPL*HAB
+  watfoo9 <- mark(supWF, nocc = nocc, model = "Nest", model.parameters = list(S = list(formula = ~ SUPPL*HAB)), groups = c("SUPPL", "HAB"), delete = T)
+  
+  return(collect.models() )
+}
+
+# run defined models
+WATFOO.results <- run.WATFOO()
+WATFOO.results
+
+
+#Save models list and est model
+save(WATFOO.results, file = "watfooGEESE.rda")
+save(watfoo7, file = "watfooGEESE_1.rda")
+save(watfoo8, file = "watfooGEESE_2.rda")
+save(watfoo6, file = "watfooGEESE_3.rda")
