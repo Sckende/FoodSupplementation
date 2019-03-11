ce191980ba25d7edbfb3c609f590c6dd5282d989
Sckende <clrccljuhasz@gmail.com>
2019-03-11 14:15:26 -0400

    Test des hypothèses pour la supplémentation en food - *** Warnings - Sortie de modèles à regarder ***

--- Shaffer method/GOOSE_SHAFFER_models.R
+++ Shaffer method/GOOSE_SHAFFER_models.R
@@ -179,7 +179,13 @@ AIC.rank(l1)
 ######################################################################
 ################### FOOD SUPPL MODELS #################################
 ##########################################################################
+#### Analysis ####
+setwd("C:/Users/HP_9470m/OneDrive - Université de Moncton/Doc doc doc/Ph.D. - ANALYSES/R analysis/Data")
 
+rm(list = ls())
+
+tot <- read.csv("GOOSE_Data_Shaffer.csv")
+summary(tot)
 # Specific database building ##############################################
 
 food <- tot[tot$YEAR == 2015 | tot$YEAR == 2016 | tot$YEAR == 2017,]
@@ -189,36 +195,116 @@ food$YEAR <- as.factor(food$YEAR)
 food$SUPPL <- relevel(food$SUPPL, "TEM")
 summary(food)
 
-# Models fitting ###########################################################
-f1 <- glm(food$Fate[food$YEAR == "2017"] ~ food$SUPPL[food$YEAR == "2017"],
-           family = binomial(link = logexp(food$EXPO[food$YEAR == "2017"])))
-summary(f1)
 
-###########################################################################
-f2 <- glm(Fate ~ HAB + SUPPL + YEAR + TEMP_Y + PREC_Y,
-          family = binomial(link = logexp(food$EXPO)),
-          data = food)
-summary(f2)
-###########################################################################
-f2.1 <- glm(Fate ~ HAB + SUPPL + lmg + TEMP_Y + PREC_Y,
-          family = binomial(link = logexp(food$EXPO)),
-          data = food)
-summary(f2.1)
-###########################################################################
-f3 <- glm(Fate ~ HAB + SUPPL + lmg,
-          family = binomial(link = logexp(food$EXPO)),
-          data = food)
-summary(f3)
-###########################################################################
-f4 <- glm(Fate ~ HAB + SUPPL,
-          family = binomial(link = logexp(food$EXPO)),
-          data = food)
-summary(f4)
-###########################################################################
-f5 <- glm(Fate ~ HAB + SUPPL + TEMP_Y * PREC_Y + lmg,
+# Function link
+logexp <- function(exposure = 1) {
+  linkfun <- function(mu) qlogis(mu^(1/exposure))
+  ## FIXME: is there some trick we can play here to allow
+  ## evaluation in the context of the 'data' argument?
+  linkinv <- function(eta) plogis(eta)^exposure
+  logit_mu_eta <- function(eta) {
+    ifelse(abs(eta)>30,.Machine$double.eps,
+           exp(eta)/(1+exp(eta))^2)
+    ## OR .Call(stats:::C_logit_mu_eta, eta, PACKAGE = "stats")
+  }
+  mu.eta <- function(eta) {
+    exposure * plogis(eta)^(exposure-1) *
+      logit_mu_eta(eta)
+  }
+  valideta <- function(eta) TRUE
+  link <- paste("logexp(", deparse(substitute(exposure)), ")",
+                sep="")
+  structure(list(linkfun = linkfun, linkinv = linkinv,
+                 mu.eta = mu.eta, valideta = valideta,
+                 name = link),
+            class = "link-glm")
+}
+
+# Function for AIC comparaison between (only) glm models 
+AIC.rank <- function(liste){
+  if(!is.list(liste))
+    stop("Argument has to be a list")
+  table <- NULL
+  for(i in 1:length(liste)){
+    
+    # if(class(liste[[i]]) %in% c("glm", "lm"))
+    #   stop("At least one model is not a glm")
+    
+    name <- liste[[i]]$formula
+    dev <- liste[[i]]$deviance
+    aic <- liste[[i]]$aic
+    
+    table <- rbind(table, c(name, dev, aic))
+    
+  }
+  # table <- as.data.frame(table)
+  
+  table <- as.data.frame(table)
+  table$V2 <- as.numeric(table$V2)
+  table$V3 <- as.numeric(table$V3)
+  
+  for(j in 1:dim(table)[1]){
+    table$V4[j] <- table$V3[j] - min(table$V3)
+  }
+  table <- table[order(table$V4),]
+  
+  names(table) <- c("Model", "Deviance", "AIC", "dAIC")
+  print(table)
+}
+
+###############################################################################
+########################### Models fitting ####################################
+###############################################################################
+foo1 <- glm(Fate ~ 1,
             family = binomial(link = logexp(food$EXPO)),
             data = food)
-summary(f5)
+summary(foo1)
+###########################################################################
+foo2 <- glm(Fate ~ SUPPL + HAB + lmg + TEMP_Y + PREC_Y,
+          family = binomial(link = logexp(food$EXPO)),
+          data = food)
+summary(foo2)
+###########################################################################
+foo2.1 <- glm(Fate ~ HAB + SUPPL + lmg + TEMP_Y,
+          family = binomial(link = logexp(food$EXPO)),
+          data = food)
+summary(foo2.1)
+###########################################################################
+foo2.2 <- glm(Fate ~ HAB + SUPPL + lmg + PREC_Y,
+              family = binomial(link = logexp(food$EXPO)),
+              data = food)
+summary(foo2.2)
+###########################################################################
+foo3 <- glm(Fate ~ HAB + lmg + TEMP_Y + PREC_Y,
+            family = binomial(link = logexp(food$EXPO)),
+            data = food)
+summary(foo3)
+###########################################################################
+foo4 <- glm(Fate ~ HAB + lmg + TEMP_Y,
+          family = binomial(link = logexp(food$EXPO)),
+          data = food)
+summary(foo4)
+###########################################################################
+foo5 <- glm(Fate ~ HAB + lmg + PREC_Y,
+            family = binomial(link = logexp(food$EXPO)),
+            data = food)
+summary(foo5)
+###########################################################################
+foo6 <- glm(Fate ~ SUPPL*TEMP_Y + SUPPL*PREC_Y + HAB + lmg,
+            family = binomial(link = logexp(food$EXPO)),
+            data = food)
+summary(foo6)
+
+##############################################################
+##################### AIC models #############################
+##############################################################
+
+l <- list(foo1, foo2, foo2.1, foo2.2, foo3, foo4, foo5, foo6)
+AIC.rank(liste = l)
+
+###### Models comparaison to test hypothesis ####################
+l1 <- list(foo1, foo6, foo3, foo2)
+AIC.rank(l1)
 
 ###########################################################################
 #################### food - 2017 ##########################################
